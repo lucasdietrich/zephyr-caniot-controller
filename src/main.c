de3850bb6430f234198f7fe0c683a2cc82307f91
Lucas Dietrich <ld.adecy@gmail.com>
2021-11-06 21:48:12 +0100

    Added button

--- src/main.c
+++ src/main.c
@@ -18,11 +18,27 @@
 #define FLAGS	0
 #endif
 
+#define SW0_NODE	DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+							      {0});
+static struct gpio_callback button_cb_data;
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+                    uint32_t pins)
+{
+        uint64_t ms = k_cyc_to_ms_floor64(k_cycle_get_32());
+	printk("Button pressed at %llu ms\n", ms);
+}
+
 void main(void)
 {
+	int ret;
+
 	const struct device *dev;
 	bool led_is_on = true;
-	int ret;
 
 	dev = device_get_binding(LED0);
 	if (dev == NULL) {
@@ -33,6 +49,31 @@ void main(void)
 	if (ret < 0) {
 		return;
 	}
+
+	if (!device_is_ready(button.port)) {
+		printk("Error: button device %s is not ready\n",
+		       button.port->name);
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, button.port->name, button.pin);
+		return;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			ret, button.port->name, button.pin);
+		return;
+	}
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
 	while (1) {
 		gpio_pin_set(dev, PIN, (int)led_is_on);
