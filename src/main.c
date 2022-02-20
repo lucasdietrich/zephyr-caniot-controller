4bdfeb053a4bad1bed8e0e093cf114ed154094e7
Lucas Dietrich <ld.adecy@gmail.com>
2022-02-20 21:33:00 +0000

    Enhanced overlay, use PWM for blue led
    - TODO red led delayed because of missing support for PWM complementary channels (e.g. TIM8_CH2N)

--- src/main.c
+++ src/main.c
@@ -12,17 +12,41 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #include <devicetree.h>
 #include <drivers/pwm.h>
 
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(tim3pwm), okay)
-#	define PWM_DEV_NODE DT_NODELABEL(tim3pwm)
+#define PWM_GREEN_LED_NODE	DT_NODELABEL(green_pwm_led)
+
+#if DT_NODE_HAS_STATUS(PWM_GREEN_LED_NODE, okay)
+#define PWM_GREEN_CTLR		DT_PWMS_CTLR(PWM_GREEN_LED_NODE)
+#define PWM_GREEN_CHANNEL	DT_PWMS_CHANNEL(PWM_GREEN_LED_NODE)
+#define PWM_GREEN_PERIOD	DT_PWMS_PERIOD(PWM_GREEN_LED_NODE)
+#define PWM_GREEN_FLAGS		DT_PWMS_FLAGS(PWM_GREEN_LED_NODE)
 #else
-#	error "PWM device not found"
+#	error "green led PWM device not found"
 #endif
+
+#define PWM_BLUE_LED_NODE	DT_NODELABEL(blue_pwm_led)
+
+#if DT_NODE_HAS_STATUS(PWM_BLUE_LED_NODE, okay)
+#define PWM_BLUE_CTLR		DT_PWMS_CTLR(PWM_BLUE_LED_NODE)
+#define PWM_BLUE_CHANNEL	DT_PWMS_CHANNEL(PWM_BLUE_LED_NODE)
+#define PWM_BLUE_PERIOD		DT_PWMS_PERIOD(PWM_BLUE_LED_NODE)
+#define PWM_BLUE_FLAGS		DT_PWMS_FLAGS(PWM_BLUE_LED_NODE)
+#else
+#	error "blue led PWM device not found"
+#endif
+
 
 static void init_pwm(void)
 {
-	const struct device *pwm_dev = DEVICE_DT_GET(PWM_DEV_NODE);
+	const struct device *green_pwm_dev = DEVICE_DT_GET(PWM_GREEN_CTLR);
 
-	int ret = pwm_pin_set_usec(pwm_dev, 3U, 1000000U, 500000U, 0);
+	int ret = pwm_pin_set_usec(green_pwm_dev, PWM_GREEN_CHANNEL, PWM_GREEN_PERIOD, 
+				   PWM_GREEN_PERIOD / 2U, PWM_GREEN_FLAGS);
+	LOG_INF("pwm_pin_set_usec returned %d", ret);
+
+	const struct device *blue_pwm_dev = DEVICE_DT_GET(PWM_BLUE_CTLR);
+
+	ret = pwm_pin_set_usec(blue_pwm_dev, PWM_BLUE_CHANNEL, PWM_BLUE_PERIOD, 
+				   PWM_BLUE_PERIOD / 2U, PWM_BLUE_FLAGS);
 	LOG_INF("pwm_pin_set_usec returned %d", ret);
 }
 
--- src/userio/leds.h
+++ src/userio/leds.h
@@ -6,13 +6,13 @@
  * 
  * - LD1 (green user led) is connected to PB0
  * 	- AF1 (alternate function 1) : TIM1_CH2N
- * 	- AF2 : TIM3_CH3
+ * 	- AF2 : TIM3_CH3 (choosen)
  * 	- AF3 : TIM8_CH2N
  * - LD2 (blue user led) is connected to PB7
- * 	- AF2 : TIM4_CH2
+ * 	- AF2 : TIM4_CH2 (choosen)
  * - LD3 (red user led) is connected to PB14
  * 	- AF1: TIM1_CH2N
- * 	- AF3 : TIM8_CH2N
+ * 	- AF3 : TIM8_CH2N (choosen)
  * 
  * - "These user LEDs are on when the I/O is HIGH value, and are off when the I/O is LOW."
  * 
