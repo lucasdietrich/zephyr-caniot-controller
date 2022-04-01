a57fc6669968947cb752cfea197164fa91ec4e00
Lucas Dietrich <ld.adecy@gmail.com>
2022-04-01 15:41:11 +0000

    Add basic support for internal temperature sensor

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <zephyr.h>
 
+#include <device.h>
+#include <drivers/sensor.h>
+
 #include "net_interface.h"
 #include "net_time.h"
 #include "crypto.h"
@@ -11,6 +14,12 @@
 
 #include <logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(st_stm32_temp)
+#define TEMP_NODE DT_INST(0, st_stm32_temp)
+#else
+#error "Could not find a compatible temperature sensor"
+#endif
 
 static void debug_mbedtls_memory(void)
 {
@@ -30,15 +39,39 @@ void main(void)
         crypto_mbedtls_heap_init();
         net_interface_init();
 
+	struct sensor_value val;
+	int rc;
+	const struct device *dev = DEVICE_DT_GET(TEMP_NODE);
+
+	if (!device_is_ready(dev)) {
+		LOG_ERR("(%p) Temperature sensor is not ready", dev);
+		return;
+	}
+
         static int counter = 0;
         
-        for (;;) {
+	for (;;) {
+		rc = sensor_sample_fetch(dev);
+		if (rc) {
+			LOG_ERR("Failed to fetch sample (%d)", rc);
+			continue;
+		}
+
+		rc = sensor_channel_get(dev, SENSOR_CHAN_DIE_TEMP, &val);
+		if (rc) {
+			LOG_ERR("Failed to get data (%d)", rc);
+			continue;
+		}
+
+		LOG_DBG("Current temperature: %.1f °C ",
+			(float)sensor_value_to_double(&val));
+
                 if (counter++ % 600 == 0) {
                         net_time_show();
 
                         debug_mbedtls_memory();
                 }
 
-                k_msleep(1000);
+                k_msleep(5000);
         }
 }
--- src/system.c
+++ src/system.c
@@ -1,5 +1,9 @@
 #include "system.h"
 
+#include <device.h>
+#include <drivers/sensor.h>
+
+
 controller_status_t controller_status = {
     .has_ipv4_addr = 0,
     .valid_system_time = 0,
