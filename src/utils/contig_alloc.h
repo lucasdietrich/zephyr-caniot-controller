e65b8582e4ac3bfe22480513408f19a7361e89d7
Lucas Dietrich <ld.adecy@gmail.com>
2022-08-20 00:07:42 +0200

    Add macro of compile-time definition of contig structures

--- src/utils/contig_alloc.h
+++ src/utils/contig_alloc.h
@@ -12,11 +12,32 @@
 
 #include <sys/dlist.h>
 
+#define Z_CONFIG_BUF_SYM(_name) \
+	(_contig_buf ## _name)
+
 struct contig {
 	size_t allocated;
 	size_t size;
-	uint8_t *buf;
+	char *buf;
 };
+
+#define CONTIG_INIT(_buf, _size) \
+	{ \
+		.allocated = 0u, \
+		.size = _size, \
+		.buf = _buf, \
+	}
+
+#define CONTIG_BUF_DEFINE(_name, _size) \
+	static __aligned(4u) char Z_CONFIG_BUF_SYM(_name)[_size];\
+	struct contig _name = { \
+		.allocated = 0u, \
+		.size = _size, \
+		.buf = Z_CONFIG_BUF_SYM(_name), \
+	}
+
+#define CONTIG_DEFINE(_name, _buf, _size) \
+	struct contig name = CONTIG_INIT(_buf, _size)
 
 int contig_init(struct contig *g, uint8_t *buffer, size_t size);
 
@@ -29,9 +50,26 @@ void *contig_alloc(struct contig *g, size_t size);
 struct kcontig {
 	size_t allocated;
 	size_t size;
-	uint8_t *buf;
+	char *buf;
 	sys_dlist_t dlist;
 };
+
+#define KCONTIG_DEFINE(_name, _buf, _size) \
+	struct kcontig name = { \
+		.allocated = 0u, \
+		.size = _size, \
+		.buf = _buf, \
+		.dlist = SYS_DLIST_STATIC_INIT(&name.dlist), \
+	}
+
+#define KCONTIG_BUF_DEFINE(_name, _size) \
+	static __aligned(4u) char Z_CONFIG_BUF_SYM(_name)[_size];\
+	struct kcontig _name = { \
+		.allocated = 0u, \
+		.size = _size, \
+		.buf = Z_CONFIG_BUF_SYM(_name), \
+		.dlist = SYS_DLIST_STATIC_INIT(&_name.dlist), \
+	}
 
 struct kcontig_block {
 	sys_dnode_t handle;
